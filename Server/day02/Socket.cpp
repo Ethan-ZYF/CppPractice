c576b394cc6f291036907241a8bd47e0d4e40422
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2025-04-08 22:08:26 -0400

    Update

--- Server/day02/Socket.cpp
+++ Server/day02/Socket.cpp
@@ -1,17 +1,17 @@
 #include "Socket.h"
+#include "InetAddress.h"
+#include "utils.h"
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "InetAddress.h"
-#include "utils.h"
 
 Socket::Socket() : fd(-1) {
     fd = socket(AF_INET, SOCK_STREAM, 0);
-    errif(fd == -1, "server socket create error");
+    errif(fd == -1, "socket create error");
 }
 
-Socket::Socket(int fd_) {
-    errif(fd == -1, "server socket create error");
+Socket::Socket(int fd_) : fd(fd_) {
+    errif(fd == -1, "socket create error");
 }
 
 Socket::~Socket() {
